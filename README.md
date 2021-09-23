6661e1a6a2d07f0de6d9b8895379a69b5fc79e6d
bluemix <a.bluemix@gmail.com>
2021-09-24 01:43:23 +0300

    fix the errors in bin/dart_application_1.dart

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+### Guide
+
+Fork this repo, and add/fix the missing code in [/bin/dart_application_1.dart](/bin/dart_application_1.dart)
+
+<img src="errors_in_dart_application_1.dart.png" width="400" alt="To be fixed errors"/>
+
--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -17,11 +17,6 @@ class View {
   }
 }
 
-class Text extends View {
-  String content;
-  Text(int id, this.content, {Color? color}) : super(id, color: color);
-}
-
 void main() {
   /*
     1) Create class named `Text` that extends/inherits `View` class,
@@ -32,26 +27,17 @@ void main() {
       var helloText = Text(<random id>, content: 'Hello' )
   */
 
-  int textId = Random().nextInt(10000);
-  Text helloText = Text(
-    textId,
-    'hello',
-  );
+  int id = Random().nextInt(10000);
 
   print('hello: $helloText');
-
-  main2();
 }
 
-void main2() {
+void task2() {
   List<int> numbers = List.generate(75, (index) => Random().nextInt(10000));
 
   /*
     Separate even numbers from the above `numbers` list.
     List<int> evenNumbers = ...
   */
-
-  List<int> evenNumbers = numbers.where((e) => e % 2 == 0).toList();
-
   print('evenNumbers: $evenNumbers');
 }
