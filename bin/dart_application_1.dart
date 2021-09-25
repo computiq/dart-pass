068f59bc5a4ad56ad96bbcba1f9fc96e90bd5db7
MohammadIq123 <mohammad.k.m.sultan@gmail.com>
2021-09-25 21:04:19 +0300

    Dart Task's solution

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -7,7 +7,7 @@ class Color {
 
 class View {
   int id;
-  Color? color;
+  Color color;
 
   View(this.id, {this.color});
 
@@ -15,6 +15,15 @@ class View {
   String toString() {
     return '$id';
   }
+}
+
+class Text extends View
+{
+
+  Text(int id, this.content, {Color color}) : super(id, color: color);
+
+  String content;
+
 }
 
 void main() {
@@ -26,18 +35,30 @@ void main() {
     2) Create new `Text` object with the following:
       var helloText = Text(<random id>, content: 'Hello' )
   */
+  
 
-  int id = Random().nextInt(10000);
+  int id2 = Random().nextInt(10000);
+  var helloText = Text( id2,  'Hello' );
 
   print('hello: $helloText');
+
+	task2();
 }
 
 void task2() {
-  List<int> numbers = List.generate(75, (index) => Random().nextInt(10000));
-
+  List<int> numbers = List.generate(10, (index) => Random().nextInt(10000));
+  print('evenNumbers: $numbers');
   /*
     Separate even numbers from the above `numbers` list.
     List<int> evenNumbers = ...
   */
-  print('evenNumbers: $evenNumbers');
+    List<int> evenNumbers = [];
+
+	 for(var i=0; i<numbers.length; i+=2)
+		 {
+    evenNumbers.add(numbers.elementAt(i));
+		 }
+     
+     print('evenNumbers: $evenNumbers');
+
 }
