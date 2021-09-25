3b0c0a8065772b10b6cf7d84b7646e96719accd3
hosin211 <41122178+hosin211@users.noreply.github.com>
2021-09-25 11:20:23 +0300

    Add/fix the missing code

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -10,34 +10,43 @@ class View {
   Color? color;
 
   View(this.id, {this.color});
-
-  @override
+@override
   String toString() {
     return '$id';
   }
+  
 }
-
+class Text extends View{
+  String? content;
+  
+          Text(int id, this.content, {Color? color}) : super(id, color: color);
+/* if we want to print the content and id 
+ * we can add this  and remove the toString method in the View class
+ @override
+  String toString() {
+    return '$content and $id';
+  }  */
+}
 void main() {
-  /*
-    1) Create class named `Text` that extends/inherits `View` class,
-        and has a `String content` property.
-        Here is the Text constuctor:
-        Text(int id, this.content, {Color? color}) : super(id, color: color)
-    2) Create new `Text` object with the following:
-      var helloText = Text(<random id>, content: 'Hello' )
-  */
 
   int id = Random().nextInt(10000);
-
+var helloText = Text(id,'Hello');
   print('hello: $helloText');
+  
 }
 
 void task2() {
   List<int> numbers = List.generate(75, (index) => Random().nextInt(10000));
+ List<int> evenNumbers=[];
 
   /*
-    Separate even numbers from the above `numbers` list.
-    List<int> evenNumbers = ...
+  Looping through the `numbers` list.
+  If the number even it will be add to the evenNumbers list 
   */
+  for (final i in numbers) {
+    if (i.isEven) {
+      evenNumbers.add(i);
+    
+  }}
   print('evenNumbers: $evenNumbers');
 }
