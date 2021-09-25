7df55b66878954ddd8f6a509fd4983fda0e31522
alialatabi <wwssr11wwss11@gmail.com>
2021-09-25 12:24:19 +0300

    Update dart_application_1.dart

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -16,6 +16,10 @@ class View {
     return '$id';
   }
 }
+class Text extends View{
+  String content;
+  Text(int id, this.content, {Color? color}) : super(id, color: color);
+  }
 
 void main() {
   /*
@@ -29,12 +33,20 @@ void main() {
 
   int id = Random().nextInt(10000);
 
+  var helloText = Text(<random id>, content: 'Hello' );
   print('hello: $helloText');
 }
 
 void task2() {
   List<int> numbers = List.generate(75, (index) => Random().nextInt(10000));
+  
+  List evenNumbers = [];
 
+  for (final i in numbers) {
+    if (i.isEven) {
+      evenNumbers.add(i);
+    } 
+  }
   /*
     Separate even numbers from the above `numbers` list.
     List<int> evenNumbers = ...
