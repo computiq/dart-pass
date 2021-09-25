4ba850b7c1ef849fe3f0256777d6d65bb6b24a5d
N4shat <91386631+N4shat@users.noreply.github.com>
2021-09-25 23:59:16 +0300

    Update dart_application_1.dart

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -16,7 +16,31 @@ class View {
     return '$id';
   }
 }
+class Text extends View {
+  String content;
+  Text(int id, this.content, {Color? color}) : super(id, color: color);
+  
 
+}
+
+void task2() {
+  List<int> numbers = List.generate(75, (index) => Random().nextInt(10000));
+  List<int> evennumbers = [];
+  for (int i in numbers){
+    if (i.isEven){
+      evennumbers.add(i);
+      
+    }
+  }
+  print("original list $numbers");
+  print("Even number List $evennumbers");
+
+  /*
+    Separate even numbers from the above `numbers` list.
+    List<int> evenNumbers = ...
+  */
+  //print('evenNumbers: $evenNumbers');
+}
 void main() {
   /*
     1) Create class named `Text` that extends/inherits `View` class,
@@ -28,16 +52,8 @@ void main() {
   */
 
   int id = Random().nextInt(10000);
-
+  var helloText = Text(id , "Hello");
   print('hello: $helloText');
+  task2();
 }
 
-void task2() {
-  List<int> numbers = List.generate(75, (index) => Random().nextInt(10000));
-
-  /*
-    Separate even numbers from the above `numbers` list.
-    List<int> evenNumbers = ...
-  */
-  print('evenNumbers: $evenNumbers');
-}
