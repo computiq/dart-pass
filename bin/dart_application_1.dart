6ecb2d21efeae7b09d929a9e8ec1ae49a6973881
Selfishmachine <91218301+Selfishmachine@users.noreply.github.com>
2021-09-25 22:38:25 +0300

    Haider Saadoun's  code

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,43 +1,39 @@
 import 'dart:math';
-
 class Color {
   Color(this.value);
   final int value;
 }
-
 class View {
   int id;
   Color? color;
-
   View(this.id, {this.color});
-
   @override
   String toString() {
     return '$id';
   }
 }
 
+
+class Text extends View{
+  String content;
+  Text(int id, this.content,{Color? color}):super(id,color: color);
+}
+
 void main() {
-  /*
-    1) Create class named `Text` that extends/inherits `View` class,
-        and has a `String content` property.
-        Here is the Text constuctor:
-        Text(int id, this.content, {Color? color}) : super(id, color: color)
-    2) Create new `Text` object with the following:
-      var helloText = Text(<random id>, content: 'Hello' )
-  */
+
 
   int id = Random().nextInt(10000);
 
+
+  Text helloText = Text(id,'Hello');
   print('hello: $helloText');
+  task2();
 }
 
 void task2() {
-  List<int> numbers = List.generate(75, (index) => Random().nextInt(10000));
+  List<int> numbers = List.generate (75, (index) => Random().nextInt(10000));
 
-  /*
-    Separate even numbers from the above `numbers` list.
-    List<int> evenNumbers = ...
-  */
+  List<int> evenNumbers = numbers.where((element) => element.isEven).toList();
+
   print('evenNumbers: $evenNumbers');
 }
