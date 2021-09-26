173af06a011535c716b80200b03f4aa9c410817b
unknown <ahmed.alyasry.gh@gmail.com>
2021-09-26 16:54:30 +0300

    Issue Fixed

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,3 +1,4 @@
+
 import 'dart:math';
 
 class Color {
@@ -17,6 +18,10 @@ class View {
   }
 }
 
+class Text extends View{
+  String content;
+  Text(int id, this.content, {Color? color}) : super(id, color: color)
+}
 void main() {
   /*
     1) Create class named `Text` that extends/inherits `View` class,
@@ -27,17 +32,18 @@ void main() {
       var helloText = Text(<random id>, content: 'Hello' )
   */
 
+  Text helloText = Text (Random.nextInt(10000), 'Hello', );
+
   int id = Random().nextInt(10000);
 
   print('hello: $helloText');
 }
-
+void main2(){
 void task2() {
   List<int> numbers = List.generate(75, (index) => Random().nextInt(10000));
 
-  /*
-    Separate even numbers from the above `numbers` list.
-    List<int> evenNumbers = ...
-  */
-  print('evenNumbers: $evenNumbers');
+  
+  List<int> Even_Numbers = numbers.where((enumber) => enumber % 2 == 0).toList();
+  print('Even_Numbers: $Even_Numbers');
+  }
 }
