04b4ed2d0c6e0ae2a32f0d8c3bb2caf6dcfccf51
Abdullah <90783453+3b00dy@users.noreply.github.com>
2021-09-26 15:10:05 +0300

    Dart Task

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -17,27 +17,26 @@ class View {
   }
 }
 
+class Text extends View {
+  String content;
+  Text(int id, this.content, {Color? color}) : super(id, color: color);
+}
+
 void main() {
-  /*
-    1) Create class named `Text` that extends/inherits `View` class,
-        and has a `String content` property.
-        Here is the Text constuctor:
-        Text(int id, this.content, {Color? color}) : super(id, color: color)
-    2) Create new `Text` object with the following:
-      var helloText = Text(<random id>, content: 'Hello' )
-  */
-
   int id = Random().nextInt(10000);
-
+  var helloText = Text(id, 'Hello');
   print('hello: $helloText');
 }
 
 void task2() {
-  List<int> numbers = List.generate(75, (index) => Random().nextInt(10000));
-
-  /*
-    Separate even numbers from the above `numbers` list.
-    List<int> evenNumbers = ...
-  */
+  List<int> numbers =
+      List<int>.generate(75, (index) => Random().nextInt(10000));
+  List<int> evenNumbers = <int>[];
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i].isEven) {
+      evenNumbers.add(numbers[i]);
+    }
+    ;
+  }
   print('evenNumbers: $evenNumbers');
 }
