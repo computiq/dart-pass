1947776e848d6b5e4d635a2ffa304e497603b8c5
unknown <zaid.haitham21@gmail.com>
2021-09-28 15:05:33 +0300

    the console resutl updated

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,49 +1,37 @@
-
 import 'dart:math';
 
 class Color {
   Color(this.value);
   final int value;
 }
-
 class View {
   int id;
   Color? color;
-
   View(this.id, {this.color});
-
+  
   @override
   String toString() {
     return '$id';
   }
 }
-
-class Test extends View{
+class Text extends View{
   String content;
-  Text(int id, this.content, {Color? color}) : super(id, color: color)
+  Text(int id, this.content, {Color? color}) : super(id, color: color);
+ 
 }
 void main() {
-  /*
-    1) Create class named `Text` that extends/inherits `View` class,
-        and has a `String content` property.
-        Here is the Text constuctor:
-        Text(int id, this.content, {Color? color}) : super(id, color: color)
-    2) Create new `Text` object with the following:
-      var helloText = Text(<random id>, content: 'Hello' )
-  */
-
-  Text helloText = Text (Random.nextInt(10000), 'Hello', );
-
+   
   int id = Random().nextInt(10000);
+  Text helloText = Text (id, 'Hello', );
 
   print('hello: $helloText');
+  task2();
 }
-void main2(){
+
+
 void task2() {
   List<int> numbers = List.generate(75, (index) => Random().nextInt(10000));
-
-  
   List<int> evenNumbers = numbers.where((enumber) => enumber % 2 == 0).toList();
   print('evenNumbers: $evenNumbers');
-  }
 }
+
