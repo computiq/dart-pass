f8ccd2eb65670c62e6fbe755f21d45bf8611ebbf
Abdullah <90783453+3b00dy@users.noreply.github.com>
2021-09-28 15:46:28 +0300

    Update dart_application_1.dart

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -26,6 +26,8 @@ void main() {
   int id = Random().nextInt(10000);
   var helloText = Text(id, 'Hello');
   print('hello: $helloText');
+  //console output {hello: 2353} this number will be change after every eun because it's random number
+  
 }
 
 void task2() {
@@ -39,4 +41,7 @@ void task2() {
     ;
   }
   print('evenNumbers: $evenNumbers');
+  //this function will show the output only when call it in main function
+  //Console output 
+  //evenNumbers: [3358, 2752, 524, 2528, 736, 4464, 3546, 8026, 2732, 5556, 5042, 746, 1728, 7732, 7952, 2166, 4750, 420, 136, 3280, 6756, 6538, 3618, 6884, 9830, 1796, 618, 7304, 1566, 8920, 9736, 7566, 6886, 3024, 9592, 2640, 3384]
 }
