384c30cacdd457364971bf8cae4efbd931df1246
Hussain Alyasiry <72931678+xbanker3@users.noreply.github.com>
2021-09-29 10:16:23 +0300

    Update dart_application_1.dart

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -23,29 +23,23 @@ class Text extends View {
 }
 
 void main() {
-  /*
-    1) Create class named Text that extends/inherits View class,
-        and has a String content property.
-        Here is the Text constructor:
-        Text(int id, this.content, {Color? color}) : super(id, color: color)
-    2) Create new Text object with the following:
-      var helloText = Text(<random id>, content: 'Hello' )
-  */
+ 
 
   int id = Random().nextInt(10000);
   var helloText = Text(id, "hussain alyasiry");
-  print('Hello: $helloText');
+  
+ print('Hello: $helloText');
+  //the output is Hello: 7303 
+  // it changes with every run 
   task2();
+  // for printing the evennumbers
 }
 
 void task2() {
   List<int> numbers = List.generate(75, (index) => Random().nextInt(10000));
   List evenNumbers = [];
 
-  /*
-    Separate even numbers from the above numbers list.
-    List<int> evenNumbers = ...
-  */
+ 
 
   for (final i in numbers) {
     if (i.isEven) {
@@ -53,4 +47,6 @@ void task2() {
     }
   }
   print('evenNumbers: $evenNumbers');
+  // the output is evenNumbers: [5846, 7814, 3402, 7126, 880, 5084, 2842, 9076, 2786, 6628, 6, 1666, 7590, 4504, 7796, 9400, 5784, 8048, 7962, 4236, 7318, 6112, 640, 5404, 9790, 9008, 1928, 388, 5942, 4250, 8156, 9284, 3154, 5408]
+  // and it changes with every run
 }
